4fc0139fe21bc0d000f541f4dc87ea47d77e698c
Marty Saxton <marty.saxton@toptal.com>
2023-10-24 20:34:53 -0500

    refactoring

---
+++ src/CircularProgress.tsx
@@ -0,0 +1,5 @@
+import './CircularProgress.css'
+
+export default function CircularProgress() {
+  return <progress className="pure-material-progress-circular" />
+}
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export * from './arguments'
 export * from './asset'
+export { default as CircularProgress } from './CircularProgress'
